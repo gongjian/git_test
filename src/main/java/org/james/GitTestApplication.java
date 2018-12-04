78d36a3832cef5e60e73954c2102572c590cfa90
gongjian <badboy.jian@gmail.com>
2018-12-04 16:31:50 +0800

    Update GitTestApplication.java

--- src/main/java/org/james/GitTestApplication.java
+++ src/main/java/org/james/GitTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitTestApplication {
-
+        // comments by master
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestApplication.class, args);
 	}
